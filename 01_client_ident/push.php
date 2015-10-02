236765d9f60d6f107d0cfe22c116d3f8eb14c55b
Nicolas Wild <n.wild@secucard.de>
2015-10-02 12:30:57 +0200

    Optimize push samples

--- 01_client_ident/push.php
+++ 01_client_ident/push.php
@@ -5,9 +5,23 @@ include "lib/init.php";
 function gotObjectPush(secucard\client\base\MainModel $obj) {
     var_dump($obj);
 }
+// simulate sample push
+$raw_post_data = '{
+    "object":"event.pushes",
+    "id":"EVT_123456789",
+    "target":"payment.secupaydebits",
+    "type":"changed",
+    "data":{
+        "object":"payment.secupaydebits",
+        "id":"xxxxxxxxx"
+    }
+}';
+//$raw_post_data = file_get_contents("php://input");
+
+$push_data = json_decode($raw_post_data);
 
 // Register function to handle new/changed objects
 $secucard->registerCallbackObject('gotObjectPush');
 
 // process request
-$secucard->processPush();
+$secucard->processPush(null, null, $push_data);
--- 02_client_payments/push.php
+++ 02_client_payments/push.php
@@ -5,20 +5,34 @@ require_once __DIR__ . '/lib/init.php';
 /**
  * Function that will handle the push
  */
-function gotObjectPush($related_obj) {
-    var_dump($related_obj);
+function gotObjectPush(secucard\client\base\MainModel $obj) {
+    var_dump($obj);
 }
 
 // Register function to handle new/changed objects pushes
 $secucard->registerCallbackObject(gotObjectPush);
 
-// simulate sample push
-$sample_push_data = '{"object":"event.pushes","id":"EVT_123456789", "target":"general.skeletons", "type":"changed", "data":{"object":"general.skeletons", "id":"SKL_WP65ADX3XUPMZFHVKKMZWCKPTP4SRM"}}';
 
-$sample_push_obj = json_decode($sample_push_data);
+
+// simulate sample push
+$raw_post_data = '{
+    "object":"event.pushes",
+    "id":"EVT_123456789",
+    "target":"payment.secupaydebits",
+    "type":"changed",
+    "data":{
+        "object":"payment.secupaydebits",
+        "id":"xxxxxxxxx"
+    }
+}';
+
+//$raw_post_data = file_get_contents("php://input");
+
+$push_data = json_decode($raw_post_data);
+
 
 try {
-	$secucard->processPush(null, null, $sample_push_obj);
+	$secucard->processPush(null, null, $push_data);
 } catch (Exception $e) {
     echo 'Error message: '. $e->getMessage() . "\n";
 }
