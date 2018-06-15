28d8ba0366c2afb068ffcdb7364795a5c60f5367
Rico Simlinger <r.simlinger@secupay.ag>
2018-06-15 14:18:42 +0200

    Update mixed basket sample

--- 05_marketplace/create_secupay_prepay_transaction.php
+++ 05_marketplace/create_secupay_prepay_transaction.php
@@ -5,9 +5,9 @@ use SecucardConnect\Product\Payment\Model\SecupayPrepay;
 use SecucardConnect\Product\Payment\Model\RedirectUrl;
 use SecucardConnect\Product\Payment\Model\Basket;
 
-const CONTRACT_ID_PLATFORM   = 'PCR_2NSCASA2N2MF75F5875XUDD87M8UA6';
-const CONTRACT_ID_MERCHANT_A = 'PCR_2TSA6QWY02MHNG9W875XU0PKC6DNA7';
-const CONTRACT_ID_MERCHANT_B = 'PCR_3XDEP6RBA2MHNGBV875XUBTKC6DNA7';
+const APIKEY_PLATFORM = '3e48bb4c1cf162c644a1f10d7e86bb83a3b1788d';
+const APIKEY_MERCHANT = '86ccab3909c993efcad51b918fc2479827940f33';
+const APIKEY_SUPPLIER = 'dd0d86a14ea00c14888b48e408851bac8d7a44de';
 
 /**
  * @var \SecucardConnect\Product\Payment\SecupayPrepaysService $service
@@ -18,38 +18,42 @@ $payment = new SecupayPrepay();
 $payment->amount = 10000;    // corresponds 100.00 EUR
 $payment->currency = 'EUR';
 $payment->purpose = 'Your purpose from TestShopName';
-$payment->order_id = '2017000123';
+$payment->order_id = '2018000123';
 $payment->customer = $customer;
 $payment->redirect_url = new RedirectUrl();
 $payment->redirect_url->url_success = 'http://shop.example.com/success.php';
 $payment->redirect_url->url_failure = 'http://shop.example.com/failure.php';
 $payment->redirect_url->url_push = 'http://shop.example.com/push.php';
 
-// Block the payout till the date of delivery (of the product or service)
+// Optional: Block the payout till the date of delivery (of the product or service)
 $payment->accrual = true;
 
 $total = 0;
 $payment->basket = [];
 
 /*
- * Define the first sub-transaction (for merchant A)
+ * Define the first sub-transaction
  */
 {
     $sub_total_1 = 0;
     $item_1 = new Basket();
-    $item_1->contract_id = CONTRACT_ID_MERCHANT_A;
+    $item_1->reference_id = '52534';
+    $item_1->apikey = APIKEY_SUPPLIER;
     $item_1->item_type = Basket::ITEM_TYPE_SUB_TRANSACTION;
-    $item_1->name = 'Position 1 of Order 000x';
+    $item_1->name = 'Position 1 aus Bestellung 000x';
 
     // Add sub basket items
     $item_1->sub_basket = [];
     {
         /*
+         *
          * Define the first sub item
+         *
          */
         $sub_item_1_1 = new Basket();
+        $sub_item_1_1->reference_id = '52534.1';
         $sub_item_1_1->quantity = 2;
-        $sub_item_1_1->name = 'Item A';
+        $sub_item_1_1->name = 'Position 1 Artikel A';
         $sub_item_1_1->ean = 'EAN001';
         $sub_item_1_1->tax = 19;
         $sub_item_1_1->price = 900;
@@ -63,19 +67,55 @@ $payment->basket = [];
         $item_1->sub_basket[] = $sub_item_1_1;
 
         /*
+         *
          * Define the second sub item
+         *
          */
         $sub_item_1_2 = new Basket();
-        $sub_item_1_2->contract_id = CONTRACT_ID_PLATFORM;
-        $sub_item_1_2->item_type = Basket::ITEM_TYPE_STAKEHOLDER_PAYMENT;
-        $sub_item_1_2->name = 'platform provision';
-        $sub_item_1_2->total = 200;
+        $sub_item_1_2->reference_id = '52534.2';
+        $sub_item_1_2->quantity = 2;
+        $sub_item_1_2->name = 'Position 1 Artikel AA';
+        $sub_item_1_2->ean = 'EAN011';
+        $sub_item_1_2->tax = 19;
+        $sub_item_1_2->price = 50;
+        $sub_item_1_2->item_type = Basket::ITEM_TYPE_ARTICLE;
 
-        // Calc sums for the second item
+        // Calc sums for the first item
+        $sub_item_1_2->total = $sub_item_1_2->quantity * $sub_item_1_2->price;
         $sub_total_1 += $sub_item_1_2->total;
 
         // Add it to the sub basket
         $item_1->sub_basket[] = $sub_item_1_2;
+
+        /*
+         *
+         * Define the first stakeholder item
+         *
+         */
+        $sub_item_1_3 = new Basket();
+        $sub_item_1_3->reference_id = '52534.3';
+        $sub_item_1_3->apikey = APIKEY_PLATFORM;
+        $sub_item_1_3->item_type = Basket::ITEM_TYPE_STAKEHOLDER_PAYMENT;
+        $sub_item_1_3->name = 'Position 1 plattform provision';
+        $sub_item_1_3->total = 200;
+
+        // Add it to the sub basket
+        $item_1->sub_basket[] = $sub_item_1_3;
+
+        /*
+         *
+         * Define the second stakeholder item
+         *
+         */
+        $sub_item_1_4 = new Basket();
+        $sub_item_1_4->reference_id = '52534.4';
+        $sub_item_1_4->apikey = APIKEY_MERCHANT;
+        $sub_item_1_4->item_type = Basket::ITEM_TYPE_STAKEHOLDER_PAYMENT;
+        $sub_item_1_4->name = 'Position 1 merchant provision';
+        $sub_item_1_4->total = 100;
+
+        // Add it to the sub basket
+        $item_1->sub_basket[] = $sub_item_1_4;
     }
 
     // Add item to the main basket
@@ -85,24 +125,28 @@ $payment->basket = [];
 }
 
 /*
- * Define the second sub-transaction (for merchant A)
+ * Define the second sub-transaction
  */
 {
     $sub_total_2 = 0;
     $item_2 = new Basket();
-    $item_2->contract_id = CONTRACT_ID_MERCHANT_A;
+    $item_2->reference_id = '52535';
+    $item_2->apikey = APIKEY_SUPPLIER;
     $item_2->item_type = Basket::ITEM_TYPE_SUB_TRANSACTION;
-    $item_2->name = 'Position 2 of Order 000x';
+    $item_2->name = 'Position 2 aus Bestellung 000x';
 
     // Add sub basket items
     $item_2->sub_basket = [];
     {
         /*
+         * 
          * Define the first sub item
+         * 
          */
         $sub_item_2_1 = new Basket();
+        $sub_item_2_1->reference_id = '52535.1';
         $sub_item_2_1->quantity = 1;
-        $sub_item_2_1->name = 'Item B';
+        $sub_item_2_1->name = 'Position 2 Artikel A';
         $sub_item_2_1->ean = 'EAN002';
         $sub_item_2_1->tax = 19;
         $sub_item_2_1->price = 1000;
@@ -116,78 +160,40 @@ $payment->basket = [];
         $item_2->sub_basket[] = $sub_item_2_1;
 
         /*
-         * Define the second sub item
+         *
+         * Define the first stakeholder item
+         *
          */
         $sub_item_2_2 = new Basket();
-        $sub_item_2_2->contract_id = CONTRACT_ID_PLATFORM;
+        $sub_item_2_2->reference_id = '52535.2';
+        $sub_item_2_2->apikey = APIKEY_PLATFORM;
         $sub_item_2_2->item_type = Basket::ITEM_TYPE_STAKEHOLDER_PAYMENT;
-        $sub_item_2_2->name = 'platform provision';
-        $sub_item_2_2->total = 100;
-
-        // Calc sums for the second item
-        $sub_total_2 += $sub_item_2_2->total;
+        $sub_item_2_2->name = 'Position 2 plattform provision';
+        $sub_item_2_2->total = 200;
 
         // Add it to the sub basket
         $item_2->sub_basket[] = $sub_item_2_2;
+
+        /*
+         *
+         * Define the second stakeholder item
+         *
+         */
+        $sub_item_2_3 = new Basket();
+        $sub_item_2_3->reference_id = '52535.3';
+        $sub_item_2_3->apikey = APIKEY_MERCHANT;
+        $sub_item_2_3->item_type = Basket::ITEM_TYPE_STAKEHOLDER_PAYMENT;
+        $sub_item_2_3->name = 'Position 2 merchant provision';
+        $sub_item_2_3->total = 100;
+
+        // Add it to the sub basket
+        $item_2->sub_basket[] = $sub_item_2_3;
     }
 
     // Add item to the main basket
     $total += $sub_total_2;
     $item_2->total = $sub_total_2;
     $payment->basket[] = $item_2;
-}
-
-/*
- * Define the third sub-transaction (for merchant B)
- */
-{
-    $sub_total_3 = 0;
-    $item_3 = new Basket();
-    $item_3->contract_id = CONTRACT_ID_MERCHANT_B;
-    $item_3->item_type = Basket::ITEM_TYPE_SUB_TRANSACTION;
-    $item_3->name = 'Position 3 of Order 000x';
-
-    // Add sub basket items
-    $item_3->sub_basket = [];
-    {
-        /*
-         * Define the first sub item
-         */
-        $sub_item_3_1 = new Basket();
-        $sub_item_3_1->quantity = 1;
-        $sub_item_3_1->name = 'Item D';
-        $sub_item_3_1->ean = 'EAN004';
-        $sub_item_3_1->tax = 19;
-        $sub_item_3_1->price = 4500;
-        $sub_item_3_1->item_type = Basket::ITEM_TYPE_ARTICLE;
-
-        // Calc sums for the first item
-        $sub_item_3_1->total = $sub_item_3_1->quantity * $sub_item_3_1->price;
-        $sub_total_3 += $sub_item_3_1->total;
-
-        // Add it to the sub basket
-        $item_3->sub_basket[] = $sub_item_3_1;
-
-        /*
-         * Define the second sub item
-         */
-        $sub_item_3_2 = new Basket();
-        $sub_item_3_2->contract_id = CONTRACT_ID_PLATFORM;
-        $sub_item_3_2->item_type = Basket::ITEM_TYPE_STAKEHOLDER_PAYMENT;
-        $sub_item_3_2->name = 'platform provision';
-        $sub_item_3_2->total = 500;
-
-        // Calc sums for the second item
-        $sub_total_3 += $sub_item_3_2->total;
-
-        // Add it to the sub basket
-        $item_3->sub_basket[] = $sub_item_3_2;
-    }
-
-    // Add item to the main basket
-    $total += $sub_total_3;
-    $item_3->total = $sub_total_3;
-    $payment->basket[] = $item_3;
 }
 
 
@@ -214,10 +220,10 @@ if ($payment->id) {
  * =======================
  *
 
-Created secupay payment transaction with id: nmmztmejxbik2409412
+Created secupay payment transaction with id: yfgocvvnlzex2937888
 Payment data: SecucardConnect\Product\Payment\Model\SecupayPrepay Object
 (
-    [transfer_purpose] => TA 10900036
+    [transfer_purpose] => TA 12351788
     [transfer_account] => SecucardConnect\Product\Payment\Model\TransferAccount Object
         (
             [account_owner] => secupay AG
@@ -232,9 +238,9 @@ Payment data: SecucardConnect\Product\Payment\Model\SecupayPrepay Object
         (
             [created] => DateTime Object
                 (
-                    [date] => 2017-12-18 10:28:49.000000
+                    [date] => 2018-06-15 14:03:04.000000
                     [timezone_type] => 1
-                    [timezone] => +01:00
+                    [timezone] => +02:00
                 )
 
             [updated] =>
@@ -244,7 +250,7 @@ Payment data: SecucardConnect\Product\Payment\Model\SecupayPrepay Object
                     [updated] =>
                     [parent] =>
                     [allow_cloning] =>
-                    [id] => PCR_2NSCASA2N2MF75F5875XUDD87M8UA6
+                    [id] => PCR_WZQJUBY092MP3JZ72F9VH9DTKGFEAH
                     [object] => payment.contracts
                 )
 
@@ -287,66 +293,116 @@ Payment data: SecucardConnect\Product\Payment\Model\SecupayPrepay Object
 
             [merchant] =>
             [merchant_customer_id] =>
-            [id] => PCU_2U6M42UE32MHNH98X75XUQ5MC6DNA7
+            [id] => PCU_3FF6BV4FU2MP3YBS2F9VHB043H52A2
             [object] => payment.customers
         )
 
     [recipient] =>
-    [amount] => 8100
+    [amount] => 2900
     [currency] => EUR
     [purpose] => Your purpose from TestShopName
-    [order_id] => 2017000123
-    [trans_id] => 10900036
+    [order_id] => 2018000123
+    [trans_id] => 12351788
     [status] => authorized
     [transaction_status] => 25
     [basket] => Array
         (
-            [0] => SecucardConnect\Product\Payment\Model\Basket Object
+            [0] => stdClass Object
                 (
-                    [quantity] =>
-                    [name] => Position 1 of Order 000x
-                    [ean] =>
-                    [tax] =>
-                    [total] => 2000
-                    [price] =>
-                    [contract_id] =>
-                    [model] =>
-                    [article_number] =>
-                    [item_type] => article
-                    [apikey] => 2f1fdbf156cc8301544dc70faf4268ec4202d5b5
-                    [sub_basket] => ...
+                    [item_type] => sub_transaction
+                    [name] => Position 1 aus Bestellung 000x
+                    [total] => 1900
+                    [apikey] => dd0d86a14ea00c14888b48e408851bac8d7a44de
+                    [reference_id] => 52534
+                    [sub_basket] => Array
+                        (
+                            [0] => stdClass Object
+                                (
+                                    [item_type] => article
+                                    [quantity] => 2
+                                    [name] => Position 1 Artikel A
+                                    [ean] => EAN001
+                                    [tax] => 19
+                                    [total] => 1800
+                                    [price] => 900
+                                    [reference_id] => 52534.1
+                                )
+
+                            [1] => stdClass Object
+                                (
+                                    [item_type] => article
+                                    [quantity] => 2
+                                    [name] => Position 1 Artikel AA
+                                    [ean] => EAN011
+                                    [tax] => 19
+                                    [total] => 100
+                                    [price] => 50
+                                    [reference_id] => 52534.2
+                                )
+
+                            [2] => stdClass Object
+                                (
+                                    [item_type] => stakeholder_payment
+                                    [name] => Position 1 plattform provision
+                                    [total] => 200
+                                    [apikey] => 3e48bb4c1cf162c644a1f10d7e86bb83a3b1788d
+                                    [reference_id] => 52534.3
+                                )
+
+                            [3] => stdClass Object
+                                (
+                                    [item_type] => stakeholder_payment
+                                    [name] => Position 1 merchant provision
+                                    [total] => 100
+                                    [apikey] => 86ccab3909c993efcad51b918fc2479827940f33
+                                    [reference_id] => 52534.4
+                                )
+
+                        )
+
                 )
 
-            [1] => SecucardConnect\Product\Payment\Model\Basket Object
+            [1] => stdClass Object
                 (
-                    [quantity] =>
-                    [name] => Position 2 of Order 000x
-                    [ean] =>
-                    [tax] =>
-                    [total] => 1100
-                    [price] =>
-                    [contract_id] =>
-                    [model] =>
-                    [article_number] =>
-                    [item_type] => article
-                    [apikey] => 2f1fdbf156cc8301544dc70faf4268ec4202d5b5
-                    [sub_basket] => ...
-                )
+                    [item_type] => sub_transaction
+                    [name] => Position 2 aus Bestellung 000x
+                    [total] => 1000
+                    [apikey] => dd0d86a14ea00c14888b48e408851bac8d7a44de
+                    [reference_id] => 52535
+                    [sub_basket] => Array
+                        (
+                            [0] => stdClass Object
+                                (
+                                    [item_type] => article
+                                    [quantity] => 1
+                                    [name] => Position 2 Artikel A
+                                    [ean] => EAN002
+                                    [tax] => 19
+                                    [total] => 1000
+                                    [price] => 1000
+                                    [reference_id] => 52535.1
+                                )
 
-            [2] => SecucardConnect\Product\Payment\Model\Basket Object
-                (
-                    [quantity] =>
-                    [name] => Position 3 of Order 000x
-                    [ean] =>
-                    [tax] =>
-                    [total] => 5000
-                    [price] =>
-                    [contract_id] =>
-                    [model] =>
-                    [article_number] =>
-                    [item_type] => article
-                    [apikey] => e01b4365254c34d8b8a5a35693a01b64b5f5b6ce
-                    [sub_basket] => ...
+                            [1] => stdClass Object
+                                (
+                                    [item_type] => stakeholder_payment
+                                    [name] => Position 2 plattform provision
+                                    [total] => 200
+                                    [apikey] => 3e48bb4c1cf162c644a1f10d7e86bb83a3b1788d
+                                    [reference_id] => 52535.2
+                                )
+
+                            [2] => stdClass Object
+                                (
+                                    [item_type] => stakeholder_payment
+                                    [name] => Position 2 merchant provision
+                                    [total] => 100
+                                    [apikey] => 86ccab3909c993efcad51b918fc2479827940f33
+                                    [reference_id] => 52535.3
+                                )
+
+                        )
+
                 )
 
         )
@@ -358,17 +414,35 @@ Payment data: SecucardConnect\Product\Payment\Model\SecupayPrepay Object
         (
             [url_success] => http://shop.example.com/success.php
             [url_failure] => http://shop.example.com/failure.php
-            [iframe_url] => https://api-testing.secupay-ag.de/payment/nmmztmejxbik2409412
+            [iframe_url] => https://api-dev6.secupay-ag.de/payment/yfgocvvnlzex2937888
             [url_push] =>
         )
 
     [url_success] =>
     [url_failure] =>
-    [iframe_url] => https://api-testing.secupay-ag.de/payment/nmmztmejxbik2409412
+    [iframe_url] => https://api-dev6.secupay-ag.de/payment/yfgocvvnlzex2937888
     [opt_data] =>
     [payment_action] => sale
     [used_payment_instrument] =>
-    [id] => nmmztmejxbik2409412
+    [sub_transactions] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [id] => yfgocvvnlzex2937888_12351789
+                    [trans_id] => 12351789
+                    [reference_id] => 52534
+                )
+
+            [1] => stdClass Object
+                (
+                    [id] => yfgocvvnlzex2937888_12351794
+                    [trans_id] => 12351794
+                    [reference_id] => 52535
+                )
+
+        )
+
+    [id] => yfgocvvnlzex2937888
     [object] => payment.secupayprepays
 )
 
--- 05_marketplace/run_sample.php
+++ 05_marketplace/run_sample.php
@@ -11,9 +11,10 @@ use SecucardConnect\Util\Logger;
 
 // Define the required config params
 $config = new ApiClientConfiguration();
+$config->setDebug(false);
 
 // Use the sandbox environment
-$config->setBaseUrl('https://connect-testing.secupay-ag.de');
+$config->setBaseUrl('https://connect-dev6.secupay-ag.de');
 
 // Create a dummy logger
 $logger = new Logger(fopen("php://stdout", "a"), false);
@@ -23,8 +24,8 @@ $store = new DummyStorage();
 
 // Create credentials storage
 $credPayments = new ClientCredentials(
-    '09ae83af7c37121b2de929b211bad944',
-    '9c5f250b69f6436cb38fd780349bc00810d8d5051d3dcf821e428f65a32724bd'
+    'f469ce017de896bbc103137f41fedfdd',
+    '6b0dc65087d4e259aac7a04781381eedd3c040b51f472522809dd6a4440985a4'
 );
 
 // Initialize the SecucardConnect client
@@ -32,8 +33,36 @@ $secucard = new SecucardConnect($config, $logger, $store, $store, $credPayments)
 
 // Create a new customer
 require 'create_customer.php';
+//$customer = "PCU_NAAWQ4KRC2MXPWRQ2F9VH98FWCAGA2";
 
 // Create a new payment transaction for credit card
 require 'create_secupay_prepay_transaction.php';
-
 // redirect the customer to the payment checkout page (iframe)
+
+
+// get details (parent TA)
+/**
+ * @var \SecucardConnect\Product\Payment\SecupayPrepaysService $service
+ */
+$service = $secucard->payment->secupayprepays;
+try {
+    $payment = $service->get($payment->id);
+    echo 'Payment data: ' . print_r($payment, true) . "\n";
+} catch (\Exception $e) {
+    echo 'Error message: ' . $e->getMessage() . "\n";
+}
+
+exit;
+
+// get details (sub-TA)
+try {
+//    $payment = $service->reverseAccrual("ashfcxwhmuam2647835_11577418");
+    $result = $service->cancel($payment->sub_transactions[0]->id, null, 190, true);
+    echo 'Cancel Payment data: ' . print_r($result, true) . "\n";
+    $payment = $service->get($payment->sub_transactions[0]->id);
+    $service->capture();
+    echo 'Payment data: ' . print_r($payment, true) . "\n";
+
+} catch (\Exception $e) {
+    echo 'Error message: ' . $e->getMessage() . "\n";
+}
